1a219e5bb017cb683bcb28aebf1df0debedd32d4
Yordan Ivanov <mail@ivanovyordan.com>
2016-10-19 19:56:10 +0300

    Read html file and render it on the page

---
+++ create-server/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var index = fs.readFile('index.html', function(error, data) {
+		if(error) {
+			return error;
+		}
+
+		res.end(data);
+	});
+});
+
+server.listen(3000);
